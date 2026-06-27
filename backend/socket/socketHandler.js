8a887109ba47b45e0b0a4775bbf7fdd102f7f59f
amirsohail22 <amir.sohail2023@ssipmt.com>
2026-06-27 19:10:41 +0530

    feat: implement Socket.io real-time messaging
    - Restructured server.js to support Socket.io (http.createServer + Server)
    - Added socketHandler with connection, join_chat, send_message, disconnect events
    - Fixed JSON string parsing issue for incoming socket data
    - Tested real-time broadcast successfully using custom HTML test client
    - Confirmed two browser tabs can exchange messages instantly via Socket.io rooms

--- backend/socket/socketHandler.js
+++ backend/socket/socketHandler.js
@@ -1,6 +1,27 @@
 const socketHandler = (io)=>{
   io.on("connection",(socket)=>{
     console.log("User connected:", socket.id);
+    // join chat
+    socket.on("join_chat",(chatId)=>{
+      socket.join(chatId);
+      console.log(`User ${socket.id} joined chat: ${chatId}`);
+    })
+
+    //sendmessage
+    socket.on("send_message" , (messageData)=>{
+      // console.log("Received messageData:", messageData);
+      // console.log("Type of messageData:", typeof messageData);
+      const data = typeof messageData === "string" ? JSON.parse(messageData) : messageData;
+
+      console.log("Parsed data:", data);
+      io.to(messageData.chatId).emit("receive_message",messageData);
+
+    })
+
+    //disconnect
+    socket.on("disconnect", () => {
+  console.log("User disconnected:", socket.id);
+});
   })
 }
 
