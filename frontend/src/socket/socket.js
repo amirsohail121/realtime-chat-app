4400bd0f23c4b22202482cb7a2b9dc0cc5b96518
amirsohail22 <amir.sohail2023@ssipmt.com>
2026-08-03 21:40:02 +0530

    fix: use production Socket.IO server URL

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
-  autoConnect: false
+export const socket = io(import.meta.env.VITE_API_URL, {
+  autoConnect: false,
+  withCredentials: true,
 });
